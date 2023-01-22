25fbbc51b2b3884c69936eee18d2fe3fe0a1770a
Dariaaal <1406dariaal@gmail.com>
2023-01-23 01:07:00 +0200

    2 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const submitBtn = document.querySelector('button');
+
+submitBtn.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(()=>{
+    if (shouldResolve) {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
 }
+
