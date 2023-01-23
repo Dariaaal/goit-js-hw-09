6278d44256f1e937f3e5c385a83204a3c6c9ee2b
Dariaaal <1406dariaal@gmail.com>
2023-01-23 15:10:05 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,30 +1,44 @@
-const submitBtn = document.querySelector('button');
-const delayInput = document.getElementsByName('delay');
-const stepInput = document.getElementsByName('step');
-const amountInput = document.getElementsByName('amount');
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
 
-submitBtn.addEventListener('submit', createPromise);
+const submitBtn = document.querySelector('button');
+const delay = document.getElementsByName('delay');
+const step = document.getElementsByName('step');
+const amount = document.getElementsByName('amount');
+
+submitBtn.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  setTimeout(()=>{
-  if (shouldResolve) {
-    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    reject(`❌ Rejected promise ${position++} in ${delay}ms`);
-  }
-}, delay);
+  const promise = new Promise((resolve, reject)=>{
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(()=>{
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay);
+  });
+  return promise;
 }
 
-createPromise(position, delay).then(result => {
-  console.log(result);
-})
-.catch(error => {
-  console.log(error);
-});
+function onSubmit(e) {
+  e.preventDefault();
+  let delayInput = +delay.value;
+  let stepInput = +step.value;
+  let amountInput = +amount.value;
 
-setInterval((createPromise(position, delay)) => {
-  Number(position)++}, stepInput.value)
+  for (let position = 1; position <= amountInput; position += 1) {
+    createPromise(position, delayInput)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      })
+    delayInput += stepInput;
+  }
+}
 
 
 
