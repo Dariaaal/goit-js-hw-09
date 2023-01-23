b9dc08fe93662ea32436648ee17d0dc2e8a008b8
Dariaaal <1406dariaal@gmail.com>
2023-01-23 02:30:51 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,32 @@
-import Notiflix from 'notiflix';
-import "notiflix/dist/notiflix-3.2.6.min.css";
-
 const submitBtn = document.querySelector('button');
+const delayInput = document.getElementsByName('delay');
+const stepInput = document.getElementsByName('step');
+const amountInput = document.getElementsByName('amount');
 
 submitBtn.addEventListener('submit', createPromise);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   setTimeout(()=>{
-    if (shouldResolve) {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } else {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
-  }, delay);
+  if (shouldResolve) {
+    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  } else {
+    reject(`❌ Rejected promise ${position++} in ${delay}ms`);
+  }
+}, delay);
 }
+
+createPromise(position, delay).then(result => {
+  console.log(result);
+})
+.catch(error => {
+  console.log(error);
+});
+
+setInterval((createPromise(position, delay)) => {
+  Number(position)++}, stepInput.value)
+
+
+
+
 
